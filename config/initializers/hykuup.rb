720268bd7df0994618dce0884af3e241bd221af3
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2025-02-17 10:26:55 -0800

    resolve rubocop warnings

--- config/initializers/hykuup.rb
+++ config/initializers/hykuup.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 # All newly-generated work types should be added to this list
 # @see SolrDocumentDecorator#hydra_model
 Rails.application.config.after_initialize do
-  HYKUUP_VALKYRIE_ONLY_MODELS = [MobiusWork, UncaWork]
+  HYKUUP_VALKYRIE_ONLY_MODELS = [MobiusWork, UncaWork].freeze
 end
