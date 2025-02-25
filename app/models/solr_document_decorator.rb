9378c51ebfd68025dc434ab6cb4be691d201054c
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2025-02-25 09:50:05 -0800

    remove override in favor of hyku's solution

--- app/models/solr_document_decorator.rb
+++ app/models/solr_document_decorator.rb
@@ -16,19 +16,6 @@ module SolrDocumentDecorator
   def file_format
     self['file_format_tesim']
   end
-
-  # OVERRIDE: Hyku v6.0.0 to prevent undefined method `active_fedora_basis_path' errors
-  def hydra_model(classifier: nil)
-    model = first('has_model_ssim')&.safe_constantize
-    # HACK: without this, #hydra_model will falsely return ActiveFedora::Base,
-    # which causes numerous undefined method `active_fedora_basis_path' errors
-    # related to polymorphic pathing. This is because, when
-    # `Hyrax.config.valkyrie_transition?` is true, Valkyrie works are expected
-    # to end in "Resource".
-    return model if HYKUUP_VALKYRIE_ONLY_MODELS.include?(model)
-
-    super
-  end
 end
 
 SolrDocument.prepend(SolrDocumentDecorator)
--- config/initializers/hykuup.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# All newly-generated work types should be added to this list
-# @see SolrDocumentDecorator#hydra_model
-Rails.application.config.after_initialize do
-  HYKUUP_VALKYRIE_ONLY_MODELS = [MobiusWork, UncaWork].freeze
-end
