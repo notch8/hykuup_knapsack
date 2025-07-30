5fa821dda027408ae9e95d19b337f7c09bbfb01c
Shana Moore <shana.lavina.moore@gmail.com>
2025-07-30 12:19:24 -0700

    use env to determine flexibility

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -4,7 +4,7 @@
 
 Rails.application.config.after_initialize do
   Hyrax.config do |config|
-    config.flexible = true
+    config.flexible = ActiveModel::Type::Boolean.new.cast(ENV.fetch('HYRAX_FLEXIBLE', false))
     config.default_m3_profile_path = HykuKnapsack::Engine.root.join('config', 'metadata_profiles', 'm3_profile.yaml')
 
     config.register_curation_concern :mobius_work
