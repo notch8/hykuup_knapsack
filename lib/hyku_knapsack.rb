2f17596f416108ff101ac8566d28d10a7f08ce2a
LaRita Robinson <larita@notch8.com>
2026-03-16 16:49:06 -0400

    Catalog translations
    Adds a spec to verify display label translations
    are working in the catalog. This spec is expected
    to fail until Hyrax changes are pulled in.
    
    Also removes unnecessary setting HYRAX_FLEXIBLE.
    This is set in docker-compose so this is redundant.

--- lib/hyku_knapsack.rb
+++ lib/hyku_knapsack.rb
@@ -2,8 +2,6 @@
 
 # This project uses flexible metadata (Hyku 7). Set before the engine is required
 # so initializers and Hyrax read the correct value.
-ENV['HYRAX_FLEXIBLE'] = 'true'
-
 # Disable include_metadata when flexible mode is enabled.
 ENV['HYRAX_DISABLE_INCLUDE_METADATA'] = 'true' if ENV.fetch('HYRAX_FLEXIBLE', 'true') == 'true'
 
