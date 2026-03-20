9adb615eaa6f0ff658600ae119913e19da367a79
LaRita Robinson <larita@notch8.com>
2026-03-20 13:59:47 -0400

    Show warning messages on m3 profile creation

--- app/controllers/hyrax/admin/metadata_profiles_controller_decorator.rb
+++ app/controllers/hyrax/admin/metadata_profiles_controller_decorator.rb
@@ -52,7 +52,10 @@ module Hyrax
         @flexible_schema = Hyrax::FlexibleSchema.create(profile: profile_data)
 
         if @flexible_schema.persisted?
-          redirect_to admin_metadata_profiles_path, notice: 'Flexible Metadata Profile was successfully created.'
+          warning_messages = @flexible_schema.warnings.full_messages.join(', ')
+          flash_options = { notice: 'Flexible Metadata Profile was successfully created.' }
+          flash_options[:alert] = warning_messages if @flexible_schema.warnings.any?
+          redirect_to admin_metadata_profiles_path, flash: flash_options
         else
           error_message = @flexible_schema.errors.messages.values.flatten.join(', ')
           redirect_to admin_metadata_profiles_path, flash: { error: error_message }
