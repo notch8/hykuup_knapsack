40ac59aaba0f7b3a7ba1e72f10a7187fd131f70d
Shana Moore <shana.lavina.moore@gmail.com>
2026-03-20 10:51:01 -0700

    Update catalog_controller_decorator.rb
    combine modules into a single decorator

--- app/controllers/catalog_controller_decorator.rb
+++ app/controllers/catalog_controller_decorator.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
-module CatalogControllerDecorator
-  extend ActiveSupport::Concern
-end
-CatalogController.prepend(CatalogControllerDecorator)
-
 # OVERRIDE: Hyku 6.1 to support linked identifiers which contain a colon
 module CatalogControllerDecorator
-  CatalogController.configure_blacklight do |config|
-    config.advanced_search[:form_facet_partial] = "advanced_search_facets"
-    key = 'identifier_tesim'
-    config.index_fields.delete(key)
-    config.add_index_field key, helper_method: :index_field_link, field_name: 'identifier_sim', if: :render_in_tenant?
+  extend ActiveSupport::Concern
+
+  prepended do
+    configure_blacklight do |config|
+      config.advanced_search[:form_facet_partial] = "advanced_search_facets"
+      key = 'identifier_tesim'
+      config.index_fields.delete(key)
+      config.add_index_field key, helper_method: :index_field_link, field_name: 'identifier_sim', if: :render_in_tenant?
+    end
   end
 end
+
+CatalogController.prepend(CatalogControllerDecorator)
