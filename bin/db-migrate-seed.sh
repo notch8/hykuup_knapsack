b7af64c43eb8554ded7e8d643467ff91f237d99c
Max Kadel <max@notch8.com>
2026-04-08 14:37:59 +0200

    Create DB using psql on deploy

--- bin/db-migrate-seed.sh
+++ bin/db-migrate-seed.sh
@@ -22,6 +22,17 @@ rescue
   []
 end
 
+def create_database(db_host, db_user, db_password, db_name)
+  check_query = "PGPASSWORD=#{db_password} psql -h #{db_host} -U #{db_user} postgres -t -c \"SELECT 1 FROM pg_database WHERE datname='#{db_name}'\""
+  result = run_command(check_query).strip
+  if result.empty?
+    puts "Creating database #{db_name}"
+    run_command("PGPASSWORD=#{db_password} psql -h #{db_host} -U #{db_user} postgres -c \"CREATE DATABASE \\\"#{db_name}\\\"\"")
+  else
+    puts "Database #{db_name} already exists"
+  end
+end
+
 def bundled_migrations
   migration_list = Bundler.load.specs.inject([]) do |arr, spec|
     if File.exist?("#{spec.full_gem_path}/lib/*/engine.rb")
@@ -52,6 +63,9 @@ begin
   service_wait("#{db_host}:#{db_port}")
   service_wait("#{fcrepo_host}:#{fcrepo_port}") if fcrepo_host
   service_wait("#{solr_host}:#{solr_port}")
+
+  # Have to create DB prior to loading Rails because the Apartment initializer calls the DB before Rails is loaded enough to run `db:create`
+  create_database(db_host, db_user, db_password, db_name)
 
   migrations_run_query = "PGPASSWORD=#{db_password} psql -h #{db_host} -U #{db_user} #{db_name} -t -c \"SELECT version FROM schema_migrations ORDER BY schema_migrations\""
   migrations_run = migrations_list(migrations_run_query)
